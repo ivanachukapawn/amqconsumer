90008abd4c2f1987ebc7d5713fd4a9c46bb1449e
John Davis <johnwdavis@gmail.com>
2017-02-28 00:29:54 -0500

    initial commit

---
+++ src/main/java/com/jms/listener/ConsumerListener.java
@@ -0,0 +1,18 @@
+package com.jms.listener;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerListener implements MessageListener
+{
+
+	public void onMessage(Message arg0)
+	{
+		System.out.println("in OnMessage");
+
+	}
+
+}
---
+++ src/test/java/test/com/jms/listener/ConsumerListenerTest.java
@@ -0,0 +1,38 @@
+package test.com.jms.listener;
+
+import static org.junit.Assert.*;
+
+import javax.jms.TextMessage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jms.listener.ConsumerListener;
+
+public class ConsumerListenerTest
+{
+	
+	private	TextMessage	message	=	null;
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	public void testOnMessage()
+	{
+		ConsumerListener	listener	=	new	ConsumerListener ();
+		listener.onMessage(message);
+		
+		assertNull(message);
+		
+	}
+
+}
